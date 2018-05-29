3278c247bb9425a08fd0e9a616274fe923ed31d4
Guillaume PEREZ <gperez@e2r11p4.42.fr>
2018-05-29 09:53:57 +0200

    check peer pas fini

--- src/peer.c
+++ src/peer.c
@@ -6,7 +6,7 @@
 /*   By: kdouveno <kdouveno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/27 13:35:13 by kdouveno          #+#    #+#             */
-/*   Updated: 2018/05/27 15:15:26 by kdouveno         ###   ########.fr       */
+/*   Updated: 2018/05/29 09:53:16 by gperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,19 @@ void	check_peer(t_env *e, t_pos *pos)
 {
 	t_peer	*pb;
 	int i;
-	int id;
 
 	i = 0;
 	while (g_meta_chars[i].c != pos->cur->obj.type)
 		i++;
 	pb = pos->peer;
-	while (pb && g_meta_chars[i].match != pb->base->obj.type)
+	while (pb && g_meta_chars[i].match != pb->base->obj.type &&
+	pos->cur->obj.meta[0] == pb->id)
 		pb = pb->next;
 	if (pb)
 	{
-		
+		pos->cur->obj.cor = 0;
+		pb->base->obj.cor = 0;
 	}
 	else
-		add_peer(e, os->pos);
+		add_peer(e, pos);
 }
