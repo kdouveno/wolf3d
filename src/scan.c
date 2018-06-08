2c7e9431eba96985b0e04c1892fd99376658a294
Guillaume <gperez@student.42fr>
2018-06-08 13:14:31 +0200

    check base resolu

--- src/scan.c
+++ src/scan.c
@@ -12,46 +12,63 @@
 
 #include "wolf3d.h"
 
-static int		check_u(t_base *b_wall)
+static int		check_u(t_base **b_wall)
 {
-	b_wall = b_wall->yu;
-	return (b_wall->next == NULL);
+	t_base *t;
+
+	t = *b_wall;
+	t = t->yu;
+	*b_wall = t;
+	return (t->next == NULL);
 }
 
-static int		check_r(t_base *b_wall)
+static int		check_r(t_base **b_wall)
 {
-	b_wall = b_wall->xu;
-	return (b_wall->next == NULL);
+	t_base *t;
+
+	t = *b_wall;
+	t = t->xu;
+	*b_wall = t;
+	return (t->next == NULL);
 }
 
-static int		check_d(t_base *b_wall)
+static int		check_d(t_base **b_wall)
 {
-	b_wall = b_wall->yd;
-	return (b_wall->next == NULL);
+	t_base *t;
+
+	t = *b_wall;
+	t = t->yd;
+	*b_wall = t;
+	return (t->next == NULL);
 }
 
-static int		check_l(t_base *b_wall)
+static int		check_l(t_base **b_wall)
 {
-	b_wall = b_wall->xd;
-	return (b_wall->next == NULL);
+	t_base *t;
+
+	t = *b_wall;
+	t = t->xd;
+	*b_wall = t;
+	return (t->next == NULL);
 }
 
-static int		check_dia(t_base *b_wall, int dir, t_vec v, t_pt *old)
+static int		check_dia(t_base **b_wall, int dir, t_vec v, t_pt *old)
 {
 	double t;
 	double x;
 	double y;
+	t_base *t_base;
 
-
-	y = (dir == UP_LEFT || dir == UP_RIGHT) ? b_wall->m.y : b_wall->m.y + 1;
+	t_base = *b_wall;
+	y = (dir == UP_LEFT || dir == UP_RIGHT) ? t_base->m.y : t_base->m.y + 1;
 	t = (y - old->y) / v.y;
 	x = v.x * t + old->x;
 
+	printf("m.y : %f m.y + 1 : %f \n",t_base->m.y , t_base->m.y + 1);
 	printf("x : %f \n",x);
 	printf("y : %f \n",y);
 	if ((int)x == (int)old->x)
 	{
-
 		if (dir == UP_LEFT)
 		{
 			if (check_u(b_wall) == 1)
@@ -128,7 +145,7 @@ static int		check_dia(t_base *b_wall, int dir, t_vec v, t_pt *old)
 }
 
 
-static int		check_wall(t_base *b_wall, int dir, t_vec v, t_pt *old)
+static int		check_wall(t_base **b_wall, int dir, t_vec v, t_pt *old)
 {
 	if (dir == UP)
 	{
@@ -200,15 +217,11 @@ int		scan(t_env *e, t_vec v)
 	int		test;
 
 	b_wall = e->cam.cur;
-
 	v = ft_norm_vec(v);
 	p = e->cam.p;
 	old = (t_pt){p.x, p.y, p.z};
 	printf("v.x = %f\n v.y = %f\n", v.x, v.y);
-	while ((test = check_wall(b_wall, check_base(v, &old, &p), v, &old)) != 1)
-	{
-		ft_putendl("on avance");
+	while ((test = check_wall(&b_wall, check_base(v, &old, &p), v, &old)) != 1)
 		ft_putnbr(test);
-	}
 	return (0);
 }
