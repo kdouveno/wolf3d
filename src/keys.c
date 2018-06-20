76ce31a2db6a980cd690fc941fcec604bda66661
Guillaume <gperez@student.42fr>
2018-06-20 17:17:45 +0200

    keys: deplacement fonctionnel

--- src/keys.c
+++ src/keys.c
@@ -12,24 +12,43 @@
 
 #include "wolf3d.h"
 
-void k_direction (int key, t_env *e)
+void	detect_wall(t_env *e, t_pt move)
 {
-	(void)key;
+	int lockx;
+	int locky;
+
+	lockx = 1;
+	locky = 1;
+	if (lockx == 1 && (int)move.x > (int)e->cam.p.x && (lockx = (e->cam.cur->xu->obj.type != 'w')))
+		e->cam.cur = e->cam.cur->xu;
+	else if (lockx == 1 && (int)move.x < (int)e->cam.p.x && (lockx = (e->cam.cur->xd->obj.type != 'w')))
+		e->cam.cur = e->cam.cur->xd;
+	if (locky == 1 && (int)move.y > (int)e->cam.p.y && (locky = (e->cam.cur->yd->obj.type != 'w')))
+		e->cam.cur = e->cam.cur->yd;
+	else if (locky == 1 && (int)move.y < (int)e->cam.p.y && (locky = (e->cam.cur->yu->obj.type != 'w')))
+		e->cam.cur = e->cam.cur->yu;
+	if(lockx == 1)
+		e->cam.p.x = move.x;
+	if(locky == 1)
+		e->cam.p.y = move.y;
+}
+
+void	k_direction (int key, t_env *e)
+{
+	t_pt move;
+
 	if (key == 0 || key == 113)
-		e->cam.p = apply(vecpro(e->cam.v_u, -0.1), e->cam.p);
+		move = apply(vecpro(e->cam.v_u, -0.1), e->cam.p);
 	else if (key == 1 || key == 115)
-	{
-	}
+		move = apply(vecpro(ft_neg_vec(e->cam.dir), MOV), e->cam.p);
 	else if (key == 2 || key == 100)
-		e->cam.p = apply(vecpro(e->cam.v_u, 0.1), e->cam.p);
+		move = apply(vecpro(e->cam.v_u, 0.1), e->cam.p);
 	else
-	{
-//	if (key == 13)
-//		e->cam.p = apply(e->cam.p, e->cam.move);
-	}
+		move = apply(vecpro(e->cam.dir, MOV), e->cam.p);
+	if (move.x > 0 && move.y > 0)
+		detect_wall(e, move);
 	algo(e);
-//	printf("x: %f y: %f z: %f\n", e->cam.p.x, e->cam.p.y, e->cam.p.z);
-//	printf("x: %f y: %f z: %f\n", e->cam.v_u.x, e->cam.v_u.y, e->cam.v_u.z);
+	printf("x: %f y: %f z: %f\n", e->cam.p.x, e->cam.p.y, e->cam.p.z);
 }
 
 void k_rotate (int key, t_env *e)
@@ -60,10 +79,7 @@ int my_key(int key, t_env *e)
 	printf("%d\n", key);
 	if (key == 0 || key == 1 || key == 2 || key == 13
 		|| key == 100 || key == 115 || key == 122 || key == 113)
-	{
-		printf("d\n");
 		k_direction(key, e);
-	}
 	else if (key == 53 || key == 65307)
 		k_escape(e);
 	else if (key == 123 || key == 65361 || key == 124 || key == 65363)
